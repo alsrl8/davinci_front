892bc2c683c8b415732756ef73881f777bc1f0c5
smg <alsrl8@naver.com>
2024-06-22 18:24:58 +0900

    New User Button 추가

--- src/components/NewUser/NewUser.tsx
+++ src/components/NewUser/NewUser.tsx
@@ -1,11 +1,23 @@
 import "./NewUser.css"
+import React, {useState} from "react";
+import NewUserModal from "./NewUserModal";
 
 const NewUser = () => {
-    return <button className="new-user-button" onClick={() => {
-        console.log("he")
-    }}>
-        New User
-    </button>
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsModalOpen(true);
+    }
+
+    return (
+        <>
+            <button className="new-user-button" onClick={handleClick}>New User</button>
+            <NewUserModal
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen}
+            />
+        </>
+    )
 }
 
 export default NewUser;
---
+++ src/components/NewUser/NewUserModal.tsx
@@ -0,0 +1,94 @@
+import {Form, Input, Modal} from 'antd';
+import {Dispatch, SetStateAction, useState} from "react";
+import "./NewUserModal.css"
+
+interface NewUserModalProps {
+    isModalOpen: boolean;
+    setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const NewUserModal = (props: NewUserModalProps) => {
+    const [userName, setUserName] = useState('myName');
+    const [userEmail, setUserEmail] = useState('myEmail');
+    const [userPassword, setUserPassword] = useState('myPw');
+    const [error, setError] = useState<string | null>(null);
+
+    const validateEmail = (email: string) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const onClickOk = async () => {
+        if (!validateEmail(userEmail)) {
+            setError('Invalid email format');
+            return;
+        }
+
+        const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
+        const urlScheme = process.env.REACT_APP_ENV === "production" ? "https" : "http";
+        if (!chatServerUrl) {
+            throw new Error("REACT_APP_CHAT_SERVER_URL environment variable is not set");
+        }
+
+        try {
+            const addUserUrl = `${urlScheme}://${chatServerUrl}/new-user`
+            const response = await fetch(addUserUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    userName: userName,
+                    userEmail: userEmail,
+                    password: userPassword
+                })
+            });
+
+            if (response.ok) {
+                console.log('Add User')
+                clearUserInput();
+                props.setIsModalOpen(false);
+            }
+        } catch (error) {
+            console.error('Error adding new user:', error);
+        }
+
+    }
+
+    const onClickCancel = () => {
+        props.setIsModalOpen(false);
+        clearUserInput();
+    }
+
+    const clearUserInput = () => {
+        setUserName('')
+        setUserEmail('')
+        setUserPassword('')
+        setError(null);
+    }
+
+    return (
+        <Modal
+            open={props.isModalOpen}
+            onOk={onClickOk}
+            onCancel={onClickCancel}
+            maskClosable={false}
+            closable={false}
+        >
+            <Form>
+                <Form.Item label="Name" className="label">
+                    <Input value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                </Form.Item>
+                <Form.Item label="Email" validateStatus={error ? "error" : ""} help={error}>
+                    <Input value={userEmail} onChange={(e) => setUserEmail(e.target.value)}/>
+                </Form.Item>
+                <Form.Item label="Password">
+                    <Input.Password value={userPassword} onChange={(e) => setUserPassword(e.target.value)}/>
+                </Form.Item>
+            </Form>
+        </Modal>
+    )
+}
+
+export default NewUserModal;
