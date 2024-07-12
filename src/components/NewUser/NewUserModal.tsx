036ed09ea645ed38383c0336403a97a9e66e5fcd
Ramsy4754 <ramsy4754@clo-guard.com>
2024-07-12 17:05:01 +0900

    User 생성 model 기능 개선

--- src/components/NewUser/NewUserModal.tsx
+++ src/components/NewUser/NewUserModal.tsx
@@ -1,4 +1,4 @@
-import {Form, Input, Modal} from 'antd';
+import {Button, Form, Input, Modal} from 'antd';
 import {Dispatch, SetStateAction, useState} from "react";
 import "./NewUserModal.css"
 
@@ -8,21 +8,66 @@ interface NewUserModalProps {
 }
 
 const NewUserModal = (props: NewUserModalProps) => {
-    const [userName, setUserName] = useState('myName');
-    const [userEmail, setUserEmail] = useState('myEmail');
-    const [userPassword, setUserPassword] = useState('myPw');
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    const [userEmailConfirmed, setUserEmailConfirmed] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    const validateEmail = (email: string) => {
+    const validateEmailFormat = (email: string) => {
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return re.test(String(email).toLowerCase());
     }
 
-    const onClickOk = async () => {
-        if (!validateEmail(userEmail)) {
-            setError('Invalid email format');
-            return;
+    const isUniqueEmail = async (email: string) => {
+        const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
+        const urlScheme = process.env.REACT_APP_ENV === "production" ? "https" : "http";
+        if (!chatServerUrl) {
+            throw new Error("REACT_APP_CHAT_SERVER_URL environment variable is not set");
         }
+
+        try {
+            const userValidationUrl = `${urlScheme}://${chatServerUrl}/user-validation`
+            const response = await fetch(userValidationUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    userEmail: userEmail
+                })
+            });
+
+            if (response.ok) {
+                return true;
+            } else {
+                const data = await response.json()
+                if (data.error !== undefined) {
+                    alert(data.error);
+                }
+                return false;
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            return false;
+        }
+    }
+
+    const validateEmail = (email: string) => {
+        if (!validateEmailFormat(email)) {
+            setError('Invalid email format');
+            setUserEmailConfirmed(false);
+        } else if (!isUniqueEmail(email)) {
+            setError('');
+            setUserEmailConfirmed(false);
+        } else {
+            setError('');
+            setUserEmailConfirmed(true);
+        }
+    }
+
+    const onClickOk = async () => {
 
         const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
         const urlScheme = process.env.REACT_APP_ENV === "production" ? "https" : "http";
@@ -48,7 +93,7 @@ const NewUserModal = (props: NewUserModalProps) => {
             if (response.ok) {
                 clearUserInput();
                 props.setIsModalOpen(false);
-            } else  {
+            } else {
                 const data = await response.json()
                 if (data.error !== undefined) {
                     alert(data.error);
@@ -69,6 +114,7 @@ const NewUserModal = (props: NewUserModalProps) => {
         setUserName('')
         setUserEmail('')
         setUserPassword('')
+        setUserEmailConfirmed(false);
         setError(null);
     }
 
@@ -79,13 +125,23 @@ const NewUserModal = (props: NewUserModalProps) => {
             onCancel={onClickCancel}
             maskClosable={false}
             closable={false}
+            okButtonProps={{disabled: !userEmailConfirmed}}
         >
             <Form>
                 <Form.Item label="Name" className="label">
                     <Input value={userName} onChange={(e) => setUserName(e.target.value)}/>
                 </Form.Item>
                 <Form.Item label="Email" validateStatus={error ? "error" : ""} help={error}>
-                    <Input value={userEmail} onChange={(e) => setUserEmail(e.target.value)}/>
+                    <div style={{display: 'flex', flexDirection: 'row'}}>
+                        <Input value={userEmail} onChange={(e) => setUserEmail(e.target.value)}
+                               disabled={userEmailConfirmed}/>
+                        <Button type="primary"
+                                onClick={() => validateEmail(userEmail)}
+                                disabled={userEmailConfirmed}
+                        >
+                            Validate Email
+                        </Button>
+                    </div>
                 </Form.Item>
                 <Form.Item label="Password">
                     <Input.Password value={userPassword} onChange={(e) => setUserPassword(e.target.value)}/>
