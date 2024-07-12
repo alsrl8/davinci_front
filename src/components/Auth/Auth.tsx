4710bf842aed65b240dad0c59d133a675b3d83fd
Ramsy4754 <ramsy4754@clo-guard.com>
2024-07-12 17:40:24 +0900

    로그인 후 성공 data를 set하는 기능 추가

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {signInWithEmailAndPassword, UserCredential} from 'firebase/auth';
 import auth from '../../config/firebaseConfig';
 import {ChatObject} from "../../types/Chat";
+import UserInfo from "../UserInfo/UserInfo";
 
 
 interface UserInfoInterface {
@@ -20,20 +21,7 @@ const Auth = (props: AuthProps) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        const idToken = sessionStorage.getItem('X-Id-Token');
-        if (idToken) {
-            try {
-                if (idToken) {
-                    loginToChatServer(idToken, "");
-                }
-            } catch (error) {
-                console.error('Failed to parse session data:', error);
-            }
-        }
-    }, []);
-
-    async function loginToChatServer(idToken: string, refreshToken: string) {
+    async function loginToChatServer(email: string, password: string) {
         const chatServerUrl = process.env.REACT_APP_CHAT_SERVER_URL;
         const urlScheme = process.env.REACT_APP_ENV === "production" ? "https" : "http";
         if (!chatServerUrl) {
@@ -46,34 +34,22 @@ const Auth = (props: AuthProps) => {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${idToken}`
                 },
                 body: JSON.stringify({
-                    refreshToken: refreshToken
+                    userEmail: email,
+                    password: password,
                 }),
                 credentials: 'include',
             });
 
             if (response.ok) {
-                console.log('Server login successful');
-
-                const idToken = response.headers.get('X-Id-Token');
-
-                if (idToken) {
-                    sessionStorage.setItem('X-Id-Token', idToken);
-                    const body = await response.json()
-                    props.setUserInfo(
-                        (prev) => {
-                            return {
-                                name: body.name,
-                                email: body.email,
-                            }
-                        }
-                    )
-                    await connectWebSocket();
-                } else {
-                    console.log("X-Id-Token doesn't exist on response header.")
-                }
+                const data = await response.json();
+                props.setUserInfo(prev => {
+                    return {
+                        name: data.name,
+                        email: data.email,
+                    }
+                })
             } else {
                 console.error('Server login failed:', response.statusText);
             }
@@ -84,10 +60,7 @@ const Auth = (props: AuthProps) => {
     }
 
     const handleLogin = async () => {
-        const userCredential: UserCredential = await signInWithEmailAndPassword(auth, email, password);
-        const idToken = await userCredential.user.getIdToken();
-        const refreshToken = userCredential.user.refreshToken;
-        await loginToChatServer(idToken, refreshToken);
+        await loginToChatServer(email, password);
     };
 
     const connectWebSocket = async () => {
